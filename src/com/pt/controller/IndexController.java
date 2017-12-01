27c7ff6ac2930738740c357f4fc1382dc83254d6
FangJieHui <448477253@qq.com>
2017-12-01 09:44:03 +0800

    change

--- src/com/pt/controller/IndexController.java
+++ src/com/pt/controller/IndexController.java
@@ -46,26 +46,18 @@ public class IndexController extends Controller {
 					+ "&" + getSignStr("website_9A39C2A8", "1B4245E3-B1F1-4F76-9D43-2856FB9DBE31\r\n" + "");
 			renderJson(responseToJsonByGet(url));
 		} else {
-			System.out.println("别特么重复按啊");
+
 		}
 	}
 
 	@ActionKey("/regist")
 	public void regist() throws Exception {
-		if (checkToken()) {
-			String url = "http://183.6.161.195:9000/api/Subscriber/Add?"
-					+ getSignStr("website_9A39C2A8", "1B4245E3-B1F1-4F76-9D43-2856FB9DBE31\r\n" + "");
-		} else {
 
-		}
 	}
 
 	@ActionKey("/getCode")
 	public void getCode() throws Exception {
-		if (checkToken()) {
-			String url = "http://183.6.161.195:9000/api/Subscriber/SendSms?"
-					+ getSignStr("website_9A39C2A8", "1B4245E3-B1F1-4F76-9D43-2856FB9DBE31\r\n" + "");
-		}
+
 	}
 
 	@ActionKey("/resetPsd")
